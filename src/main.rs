55134350369a6019d4d4b4df836f0599d1d5e94f
haris <harismuzaffer@gmail.com>
2024-11-16 20:07:45 +0530

    feat: a very crude version of tokio based atr

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,12 @@ use std::{
 };
 
 use clap::Parser;
+use futures::{stream::FuturesUnordered, StreamExt};
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
+use tokio::io::{unix::AsyncFd, Interest};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("starting atr...");
 
     let args = Args::parse();
@@ -19,23 +22,29 @@ fn main() {
     let target_addrs = resolve_host_name(target_host);
     println!("target_ips are {:?}", target_addrs);
     let target_addr = target_addrs[0];
-    let timeout = Duration::from_secs(1);
-    let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP)).unwrap();
-    println!("socket created {:?} -> ", socket);
 
+    let mut futures = FuturesUnordered::new();
     for ttl in 1..64 {
-        socket.set_ttl(ttl).unwrap();
-        socket.set_read_timeout(Some(timeout)).unwrap();
-        socket.set_write_timeout(Some(timeout)).unwrap();
-        let info: Info = send_packet_t(&socket, target_addr);
-        println!("{}", info);
-        match info._status {
-            Status::DONE => {
-                break;
+        let future = send_packet_t(ttl, target_addr);
+        futures.push(future);
+    }
+    loop {
+        tokio::select! {
+            r = futures.next() => {
+                match r.flatten() {
+                    Some(i) => {
+                        println!("Info: {}", i);
+                    }
+                    None => {
+                        println!("No Info");
+                        break;
+                    }
+                }
             }
-            _ => {
-                continue;
-            }
+            else => {
+                println!("nothing matched, breaking");
+                break
+            },
         }
     }
 }
@@ -46,43 +55,67 @@ fn resolve_host_name(host_name: &str) -> Vec<SocketAddr> {
     ips
 }
 
-fn send_packet_t(socket: &Socket, addr: SocketAddr) -> Info {
+async fn send_packet_t(ttl: u32, addr: SocketAddr) -> Option<Info> {
+    let timeout = Duration::from_secs(3);
+    let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP)).unwrap();
+    socket.set_read_timeout(Some(timeout)).unwrap();
+    socket.set_write_timeout(Some(timeout)).unwrap();
+    socket.set_nonblocking(true).unwrap();
+    socket.set_ttl(ttl).unwrap();
     let t_start = Instant::now();
-    let info: Info;
-    match socket.connect(&SockAddr::from(addr)) {
-        Ok(_resp) => {
-            info = Info {
-                tt: t_start.elapsed().as_millis_f32(),
-                _status: Status::DONE,
-                status_line: String::from("DONE"),
-            };
+
+    socket.connect(&SockAddr::from(addr));
+
+    let async_fd = AsyncFd::new(socket).unwrap();
+    let timeout = Duration::from_secs(3);
+    let r = tokio::time::timeout(timeout, async {
+        async_fd
+            .async_io(Interest::WRITABLE, |socket| socket.take_error())
+            .await
+    })
+    .await;
+
+    match r {
+        Err(_) => {
+            println!("Timeout");
+            None
         }
-        Err(error_info) => match error_info.kind() {
-            io::ErrorKind::ConnectionRefused => {
-                info = Info {
+        Ok(result) => {
+            let info = match result {
+                Ok(None) => Info {
                     tt: t_start.elapsed().as_millis_f32(),
-                    _status: Status::ERR,
-                    status_line: String::from("*"),
-                };
-            }
-            io::ErrorKind::HostUnreachable => {
-                info = Info {
-                    tt: t_start.elapsed().as_millis_f32(),
-                    _status: Status::OK,
-                    status_line: String::from("OK"),
-                };
-            }
-            _ => {
-                info = Info {
+                    _status: Status::DONE,
+                    status_line: String::from("DONE"),
+                },
+                Ok(Some(error_info)) => {
+                    // println!("error info {:?}", error_info);
+                    match error_info.kind() {
+                        io::ErrorKind::ConnectionRefused => Info {
+                            tt: t_start.elapsed().as_millis_f32(),
+                            _status: Status::ERR,
+                            status_line: String::from("*"),
+                        },
+                        io::ErrorKind::HostUnreachable => Info {
+                            tt: t_start.elapsed().as_millis_f32(),
+                            _status: Status::OK,
+                            status_line: String::from("OK"),
+                        },
+                        _ => Info {
+                            tt: t_start.elapsed().as_millis_f32(),
+                            _status: Status::ERR,
+                            status_line: String::from("***"),
+                        },
+                    }
+                }
+                Err(_) => Info {
                     tt: t_start.elapsed().as_millis_f32(),
                     _status: Status::ERR,
                     status_line: String::from("***"),
-                };
-            }
-        },
+                },
+            };
+            Some(info)
+        }
     }
-
-    info
 }
 
 struct Info {
